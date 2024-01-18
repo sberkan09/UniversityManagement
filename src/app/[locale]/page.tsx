b66b98acc59db9a1f3df8b1732205f6ab1e1c098
sberkan09 <samiakkaya396@gmail.com>
2024-01-18 21:39:41 +0300

    Localization altyapısı

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,21 @@
 import {useTranslations} from 'next-intl';
- 
-export default function Index() {
+import Link from 'next/link';
+
+function LanguageButton({ params }: { params: { locale: string } }) {
+    if (params.locale === "en") {
+        return (<Link href="/tr"><button className='w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>tr</button></Link>)
+    } else if (params.locale === "tr") {
+        return (<Link href="/en"><button className='w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>en</button></Link>)
+    }
+}
+
+export default function Index({ params }: { params: { locale: string } }) {
   const t = useTranslations('Index');
-  return <h1>{t('title')}</h1>;
+
+  return (
+    <center>
+        <h1>{t('title')}</h1>
+        <LanguageButton params={params}/>
+    </center>
+  )
 }
